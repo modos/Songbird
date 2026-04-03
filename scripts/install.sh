bc05da6e7f4091feeb737f37fb0caeca5ccc5ff3
Pouya Khalili <pouyakhalili83@gmail.com>
2026-04-03 15:02:28 +0330

    hotfix(install): install script issues - part 4

--- scripts/install.sh
+++ scripts/install.sh
@@ -621,7 +621,7 @@ clone_repo() {
     return 0
   fi
 
-  if run_as_root test -n "$(run_as_root_output find "$INSTALL_DIR" -mindepth 1 -maxdepth 1 ! -name "logs" ! -name "data" -print -quit)"; then
+  if run_as_root test -n "$(run_as_root_output find "$INSTALL_DIR" -mindepth 1 -maxdepth 1 -print -quit)"; then
     if [[ "$(prompt_yes_no "${INSTALL_DIR} exists and is not empty. Delete it and re-clone from GitHub?" "no")" != "yes" ]]; then
       warn "Installation canceled. Clear ${INSTALL_DIR} or use offline mode."
       press_enter_to_continue
